cb0737dc1bc22af6fcc6af35ff54df51c4af4ff4
Jeremy Frank <jeremyjfrank@gmail.com>
2022-05-04 19:20:11 -0400

    Add tailwind integration and files from create-astro

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
-  content: [
-    "./public/**/*.html",
-    "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}",
-  ],
-};
+  content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
