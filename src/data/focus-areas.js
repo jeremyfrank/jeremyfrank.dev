3883d781dc9440ec8bb72d34c6dbe3d10d4a29a4
Jeremy Frank <jeremyjfrank@gmail.com>
2022-05-04 21:41:25 -0400

    Add focus areas

---
+++ src/data/focus-areas.js
@@ -0,0 +1,20 @@
+export const focusAreas = [
+  {
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" class="h-16 w-16"><circle cx="12" cy="12" r="12"/></svg>',
+    title: 'UI Development',
+    description: 'Collaborating on and implementing user interface patterns and design systems in an extendable, durable and maintainable way.',
+    favorites: ['HTML', 'CSS', 'JavaScript', 'Tailwind', 'Storybook'],
+  },
+  {
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" class="h-16 w-16"><circle cx="12" cy="12" r="12"/></svg>',
+    title: 'Performance',
+    description: 'Fostering awareness about web performance and implementing best practices in measurement, optimization and monitoring.',
+    favorites: ['CrUX', 'Lighthouse', 'PageSpeed Insights'],
+  },
+  {
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" class="h-16 w-16"><circle cx="12" cy="12" r="12"/></svg>',
+    title: 'Accessibility',
+    description: 'Advocating for accessibility and implementing best practices for screen readers and keyboard use.',
+    favorites: ['VoiceOver', 'axe', 'WCAG'],
+  },
+]
