4e26e1f88608c1aa20b53daede9c940084481e3a
Jeremy Frank <jeremyjfrank@gmail.com>
2022-05-07 19:57:06 -0400

    Add select clients section

---
+++ src/data/clients.js
@@ -0,0 +1,117 @@
+export const clients = [
+  {
+    name: 'AHIP',
+    image: 'ahip.svg',
+  },
+  {
+    name: 'Urū Sports',
+    image: 'uru.svg',
+  },
+  {
+    name: 'Ad Council',
+    image: 'adcouncil.svg',
+  },
+  {
+    name: 'Standard Cognition',
+    image: 'standard.svg',
+  },
+  {
+    name: 'Gooten',
+    image: 'gooten.svg',
+  },
+  {
+    name: 'National Trust for Historic Preservation',
+    image: 'nthp.svg',
+  },
+  {
+    name: 'Center for a New American Security',
+  },
+  {
+    name: 'University of Chicago',
+    image: 'uchicago.svg',
+  },
+  {
+    name: 'Shedd Aquarium',
+    image: 'shedd.svg',
+  },
+  {
+    name: 'The Nature Conservancy',
+  },
+  {
+    name: 'Conservation International',
+  },
+  {
+    name: 'American Chemical Society',
+  },
+  {
+    name: 'Cradle To Cradle',
+  },
+  {
+    name: 'Craig Hospital',
+  },
+  {
+    name: 'Human Rights Campaign',
+  },
+  {
+    name: 'OnGood',
+  },
+  {
+    name: 'iContact',
+  },
+  {
+    name: 'Politico',
+    image: 'politico.svg',
+  },
+  {
+    name: 'University of Pennsylvania',
+    image: 'upenn.svg',
+  },
+  {
+    name: 'Discovery',
+  },
+  {
+    name: 'Avizia',
+  },
+  {
+    name: 'Dick’s Sporting Goods',
+  },
+  {
+    name: 'Massachusetts General Hospital',
+  },
+  {
+    name: 'Nest Realty',
+  },
+  {
+    name: 'Wildlife Conservation Society',
+  },
+  {
+    name: 'World Monuments Fund',
+  },
+  {
+    name: 'Johns Hopkins University',
+  },
+  {
+    name: 'Puma',
+  },
+  {
+    name: 'Lenovo',
+  },
+  {
+    name: 'Adaptive Path',
+  },
+  {
+    name: 'Get Satisfaction',
+  },
+  {
+    name: 'Mount Nittany Medical Center',
+  },
+  {
+    name: 'Hershey Entertainment',
+  },
+  {
+    name: 'Milton Hershey School',
+  },
+  {
+    name: 'Winestore',
+  },
+]
