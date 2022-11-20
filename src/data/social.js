7413cc77e0efc30461814b20a26433dbf4da747d
Jeremy Frank <jeremyjfrank@gmail.com>
2022-11-20 16:01:02 -0500

    Add social icons bar

---
+++ src/data/social.js
@@ -0,0 +1,17 @@
+export const social = [
+  {
+    name: 'LinkedIn',
+    image: 'linkedin.svg',
+    url: 'https://www.linkedin.com/in/jerfrank/',
+  },
+  {
+    name: 'GitHub',
+    image: 'github.svg',
+    url: 'https://github.com/jeremyfrank/',
+  },
+  {
+    name: 'Codepen',
+    image: 'codepen.svg',
+    url: 'https://codepen.io/jeremyfrank/',
+  },
+]
