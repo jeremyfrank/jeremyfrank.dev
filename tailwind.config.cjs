58af573d39ab0e85d3d707fb22656b25f761b594
Jeremy Frank <jeremyjfrank@gmail.com>
2023-11-10 15:12:33 -0500

    Uninstall tailwindcss/typography

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,93 +19,6 @@ module.exports = {
       gridTemplateColumns: {
         inherit: 'inherit',
       },
-      typography: {
-        DEFAULT: {
-          css: {
-            maxWidth: 'none',
-            a: {
-              fontWeight: null,
-            },
-            h2: {
-              fontWeight: null,
-            },
-            h3: {
-              fontWeight: null,
-            },
-            li: {
-              marginTop: '0.75em',
-              marginBottom: '0.75em',
-              lineHeight: 1.5,
-            },
-            ':not(pre) > code': {
-              backgroundColor: 'rgba(0,0,0,0.1)',
-              borderRadius: '0.15em',
-              fontWeight: 'normal',
-              padding: '0.1em 0.2em',
-            },
-            'code::before': {
-              content: 'none',
-            },
-            'code::after': {
-              content: 'none',
-            },
-            iframe: {
-              marginTop: em(24),
-              marginBottom: em(24),
-            },
-          },
-        },
-        lg: {
-          css: {
-            h2: {
-              fontSize: '1.5em',
-              marginTop: '1.5em',
-              marginBottom: '0.75em',
-            },
-            h3: {
-              fontSize: '1.2em',
-              marginTop: '1.5em',
-              marginBottom: '0.75em',
-            },
-            li: {
-              marginTop: '0.75em',
-              marginBottom: '0.75em',
-            },
-            iframe: {
-              marginTop: '2em',
-              marginBottom: '2em',
-            },
-          },
-        },
-        xl: {
-          css: {
-            h2: {
-              fontSize: '1.5em',
-              marginTop: '1.5em',
-              marginBottom: '0.75em',
-            },
-            h3: {
-              fontSize: '1.2em',
-              marginTop: '1.5em',
-              marginBottom: '0.75em',
-            },
-            li: {
-              marginTop: '0.75em',
-              marginBottom: '0.75em',
-            },
-          },
-        },
-        invert: {
-          css: {
-            '--tw-prose-hr': colors.zinc[500],
-          },
-        },
-        zinc: {
-          css: {
-            '--tw-prose-hr': colors.zinc[300],
-          },
-        },
-      },
     },
     fontFamily: {
       sans: ['"Calluna Sans"', 'system-ui', 'ui-sans-serif', 'sans-serif'],
@@ -120,5 +33,4 @@ module.exports = {
       xl: em(1280),
     },
   },
-  plugins: [require('@tailwindcss/typography')],
 }
