b1ba4fe3cc9db67be1923b17d3ed2c659b613237
Jeremy Frank <jeremyjfrank@gmail.com>
2024-01-13 17:35:56 -0500

    Add logo marquee

---
+++ src/content/craft/logo-marquee/_assets/logos.js
@@ -0,0 +1,52 @@
+export const logos = [
+  {
+    srcDark: '../_assets/arc-dark.svg',
+    srcLight: '../_assets/arc-light.svg',
+    alt: 'Arc',
+  },
+  {
+    srcDark: '../_assets/frame-io-dark.svg',
+    srcLight: '../_assets/frame-io-light.svg',
+    alt: 'Frame.io',
+  },
+  {
+    srcDark: '../_assets/linear-dark.svg',
+    srcLight: '../_assets/linear-light.svg',
+    alt: 'Linear',
+  },
+  {
+    srcDark: '../_assets/mercury-dark.svg',
+    srcLight: '../_assets/mercury-light.svg',
+    alt: 'Mercury',
+  },
+  {
+    srcDark: '../_assets/openai-dark.svg',
+    srcLight: '../_assets/openai-light.svg',
+    alt: 'OpenAI',
+  },
+  {
+    srcDark: '../_assets/ramp-dark.svg',
+    srcLight: '../_assets/ramp-light.svg',
+    alt: 'Ramp',
+  },
+  {
+    srcDark: '../_assets/raycast-dark.svg',
+    srcLight: '../_assets/raycast-light.svg',
+    alt: 'Raycast',
+  },
+  {
+    srcDark: '../_assets/rive-dark.svg',
+    srcLight: '../_assets/rive-light.svg',
+    alt: 'Rive',
+  },
+  {
+    srcDark: '../_assets/stripe.svg',
+    srcLight: '../_assets/stripe-light.svg',
+    alt: 'Stripe',
+  },
+  {
+    srcDark: '../_assets/vercel-dark.svg',
+    srcLight: '../_assets/vercel-light.svg',
+    alt: 'Vercel',
+  },
+]
