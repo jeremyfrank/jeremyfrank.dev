5ea95de571e80b3d739b8fc7c2da54372171772c
Jeremy Frank <jeremyjfrank@gmail.com>
2026-06-30 21:23:48 -0400

    Update to Astro 7

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
+  // Preserve Astro 6 whitespace handling following HTML rules, while still compressing HTML output
+  // https://docs.astro.build/en/guides/upgrade-to/v7/#new-default-whitespace-handling-compresshtml-jsx
+  compressHTML: true,
   image: {
     domains: ['ucarecdn.com'],
   },
